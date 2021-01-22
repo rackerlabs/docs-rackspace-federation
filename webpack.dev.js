c08ea59b94dbf4d63625eb35f2cca108be06fbfa
Ravi <raviteja.valluri@rackspace.com>
2021-01-22 09:01:29 -0800

    New Directory Structure

---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const exec = require("child_process").exec;
+const ShellPlugin = require("webpack-shell-plugin-next");
+const common = require("./webpack.common.js");
+
+
+module.exports = merge(common, {
+  mode: "development",
+  watch: true,
+  devServer: {
+    contentBase: "doc/_build/html",
+    port: 1919,
+    open: false,
+    hot: false,
+    liveReload: true,
+    publicPath: "/_static/",
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  plugins: [
+    new ShellPlugin({
+      onBuildStart: ["cd doc && make html && cd .."],
+    })
+  ]
+});
